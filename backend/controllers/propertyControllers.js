efe6713ed5f452a837aea281dace0dd422017b87
Renan Horuz <renanh@metropolia.fi>
2026-03-09 10:33:48 +0200

    [iter3] feat(properties): implement PUT /api/properties/:id to update a property

--- backend/controllers/propertyControllers.js
+++ backend/controllers/propertyControllers.js
@@ -1,5 +1,12 @@
 const Property = require("../models/propertyModel");
 const mongoose = require("mongoose")
+
+const validateId = (id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new Error("Invalid property Id");
+  }
+}
+
 // GET /api/properties
 const getAllProperties = async (req, res) => {
   try {
@@ -26,36 +33,39 @@ const createProperty = async (req, res) => {
 
 // GET /api/properties/:propertyId
 const getPropertyById = async (req, res) => {
-  try{
-    if(!mongoose.Types.ObjectId.isValid(req.params.propertyId)){
-      return res.status(400).json({error: "Invalid property Id"});
-    }
+  try {
+    validateId(req.params.propertyId)
     const property = await Property.findById(req.params.propertyId);
-
-    if (!property){
-      return res.status(404).json({error:"Property not found"})
-
+    if (!property) {
+      return res.status(404).json({ error: "Property not found" })
     }
     res.status(200).json(property);
 
-  }catch (error){
-    res.status(500).json({error: error.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
- 
+
 };
 
 // PUT /api/properties/:propertyId
 const updateProperty = async (req, res) => {
-  res.send("updateProperty");
+  const id = req.params.propertyId
+  validateId(id)
+  try {
+    const updatedProperty = await Property.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true })
+    if (updatedProperty) {
+      return res.status(200).json({ message: "Updated property successfully", output: updatedProperty })
+    }
+    return res.status(404).json({ error: "No property to update" })
+  } catch (e) {
+    return res.status(500).json({ error: `ERROR updating property: ${e}` })
+  }
 };
 
 // DELETE /api/properties/:propertyId
 const deleteProperty = async (req, res) => {
-   try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.propertyId)) {
-      return res.status(400).json({ error: "Invalid property ID" });
-    }
-
+  try {
+    validateId(req.params.propertyId)
     const property = await Property.findByIdAndDelete(req.params.propertyId);
 
     if (!property) {
